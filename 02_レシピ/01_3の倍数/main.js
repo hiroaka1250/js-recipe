4ae51b471cc5129a0e4f33f4480aa49fda58d2e6
akahon <hiroaka1250@gmail.com>
2022-01-16 17:54:40 +0900

    [FizzBuZZ]に取り組みました。

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -26,3 +26,23 @@ const genkiFunction = function (number) {
     }
   }
 }
+
+const fizBuZZ = function (number) {
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0) {
+      if (n % 5 === 0) {
+        console.log("FizzBuzz")
+      } else {
+        console.log("Fizz")
+      }
+    } else if (n % 5 === 0) {
+      if (n % 3 === 0) {
+        console.log("FizzBuzz")
+      } else {
+        console.log("Buzz")
+      }
+    } else {
+      console.log(n)
+    }
+  }
+}
