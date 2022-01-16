e71765255ffeaa4c7b737d8e7ca4f1c5abd2c212
akahon <hiroaka1250@gmail.com>
2022-01-16 17:29:34 +0900

    [カウンター]を学習しました。

---
+++ 02_レシピ/01_カウンター/main.js
@@ -0,0 +1,10 @@
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
+
+let count = 0
+// ボタンをクリックしたときの処理を登録
+plusButton.onclick = function () {
+  // ここにクリック後の処理を書く
+  count += 1
+  display.textContent = count
+}
