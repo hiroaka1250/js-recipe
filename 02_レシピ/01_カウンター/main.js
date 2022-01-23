c2161a1fdb965e8aef186f9a15d0f37b60084c26
akahon <hiroaka1250@gmail.com>
2022-01-23 17:41:21 +0900

    [便利なボタン]を学習しました

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,10 +1,21 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const doubleButton = document.getElementById("double-button")
 
 let count = 0
 // ボタンをクリックしたときの処理を登録
 plusButton.onclick = function () {
   // ここにクリック後の処理を書く
   count += 1
+  display.textContent = count
+}
+minusButton.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+
+doubleButton.onclick = function () {
+  count *= 2
   display.textContent = count
 }
