3e021a117f6814172497efcfc2abf85cd1c63e4a
akahon <hiroaka1250@gmail.com>
2022-01-23 19:08:28 +0900

    ゼミで自販機を作りました

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -2,20 +2,32 @@ const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
 const minusButton = document.getElementById("minus-button")
 const doubleButton = document.getElementById("double-button")
+const dengenButton = document.getElementById("dengen-button")
 
 let count = 0
+let dengen = false
 // ボタンをクリックしたときの処理を登録
+dengenButton.onclick = function () {
+  dengen = !dengen // !は否定→状態の否定をクリックするたびに更新する
+  dengenButton.textContent = dengen ? "電源OFF" : "電源ON"
+}
 plusButton.onclick = function () {
   // ここにクリック後の処理を書く
-  count += 1
-  display.textContent = count
+  if (dengen) {
+    count += 1
+    display.textContent = count
+  }
 }
 minusButton.onclick = function () {
-  count -= 1
-  display.textContent = count
+  if (dengen) {
+    count -= 1
+    display.textContent = count
+  }
 }
 
 doubleButton.onclick = function () {
-  count *= 2
-  display.textContent = count
+  if (dengen) {
+    count *= 2
+    display.textContent = count
+  }
 }
