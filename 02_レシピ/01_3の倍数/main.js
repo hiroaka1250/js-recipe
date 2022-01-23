8fe983570d13bd337f90096b2aaaeca7939e37a0
akahon <hiroaka1250@gmail.com>
2022-01-23 17:34:45 +0900

    [3が大好き]、[元気な秒針]を学習しました

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -29,20 +29,40 @@ const genkiFunction = function (number) {
 
 const fizBuZZ = function (number) {
   for (let n = 1; n <= number; n++) {
-    if (n % 3 === 0) {
-      if (n % 5 === 0) {
-        console.log("FizzBuzz")
-      } else {
-        console.log("Fizz")
-      }
+    if (n % 15 === 0) {
+      console.log("FizzBuzz")
+    } else if (n % 3 === 0) {
+      console.log("Fizz")
     } else if (n % 5 === 0) {
-      if (n % 3 === 0) {
-        console.log("FizzBuzz")
-      } else {
-        console.log("Buzz")
-      }
+      console.log("Buzz")
     } else {
       console.log(n)
     }
   }
+}
+
+const genkiFunction2 = function (number) {
+  for (let n = 1; n <= number; n++) {
+    const stringNumbers = String(n).split("")
+    if (n % 3 === 0) {
+      console.log(`${n}!!!!!!!`)
+    } else if (stringNumbers.includes("3")) {
+      console.log(`${n}!!!!!!!`)
+    } else {
+      console.log(n)
+    }
+  }
+}
+
+let startNumber = 0
+const countUp = function () {
+  startNumber += 1
+  if (startNumber % 3 === 0) {
+    console.log(`${startNumber}!!!!!`)
+  } else {
+    console.log(startNumber)
+  }
+}
+const genkiFunction3 = function () {
+  setInterval(countUp, 1000)
 }
